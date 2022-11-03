f9066fd7936af5bc731cf727f6be1c60bc5d7c6b
alan lew <alan@ethereal.cc>
2022-11-03 13:07:28 -0400

    Fix CSS to move feedback widget to the bottom of the page regardless of the page content length; Make feedback modal and confirmation popup fade in and fade out - feedback modal doesn't fade out yet; Move feedback.js reference to the feedback partial template;

--- site/static/js/feedback.js
+++ site/static/js/feedback.js
@@ -4,45 +4,138 @@
 let eventValue = 0;
 let rating = '';
 
-$('body').on('click', '.c-thermometer__emojis a', function () {
-    $(this).addClass('selected');
-});
-
-$('body').on('click', '.c-thermometer-modal__footer .btn', function () {
-    if ($(this).hasClass('btn-link')) {
-        $(this).parents('.c-thermometer-modal__container').fadeOut();
-        $('a.rate-this-page-action').removeClass('selected');
-        $('.c-thermometer-modal__container textarea').val('');
-    } else {
-        const currentString = $(".c-thermometer-modal__container textarea").val()
-
-        if (eventValue > 0) {
-            // Submit DataLayer Event
-            const dataLayer = window.dataLayer || [];
-            dataLayer.push({
-                event: 'rateThisPage',
-                eventLabel: rating,
-                eventValue: eventValue,
-                eventFeedback: currentString
-            });
-
-            if (typeof (rudderanalytics) !== 'undefined') {
-                rudderanalytics.track("feedback_submitted", { label: rating, rating: eventValue, feedback: currentString });
+/** Add click handlers to each of the emoji buttons */
+const addEmojiClick = () => {
+    const emojis = document.getElementsByClassName("rate-this-page-action");
+    for (const emojiEl of emojis) {
+        emojiEl.addEventListener('click', (e) => {
+            emojiEl.classList.add("selected");
+            showThermometerModal();
+            // Prepare DataLayer Vars
+            rating = emojiEl.getAttribute("data-rating");
+            if (rating !== null) {
+                switch (rating) {
+                    case 'Yes':
+                        eventValue = 3;
+                        break;
+                    case 'Somewhat':
+                        eventValue = 2;
+                        break;
+                    case 'No':
+                        eventValue = 1;
+                        break;
+                }
             }
-
-            $(this).parents('.c-thermometer-modal__container').fadeOut(() => {
-                $('.c-thermometer-modal__container').remove();
-            });
-            $('.c-thermometer__emojis').addClass('pointer-events-none');
-
-            $('.c-thermometer-popup').fadeIn();
-
-            setTimeout(() => {
-                $('.c-thermometer-popup').fadeOut('fast');
-            }, 3000)
-        }
+        });
     }
-});
+};
+
+/** Reset the state of the emoji buttons to default */
+const resetEmojiStates = () => {
+    const emojis = document.getElementsByClassName("rate-this-page-action");
+    for (const emojiEl of emojis) {
+        emojiEl.classList.remove("selected");
+    }
+};
+
+/** Hide the confirmation popup */
+const hideConfirmationPopup = () => {
+    const confirmationEls = document.getElementsByClassName("c-thermometer-popup");
+    if (confirmationEls.length > 0) {
+        confirmationEls[0].classList.replace("show", "hide");
+    }
+};
+
+/** Show the confirmation popup */
+const showConfirmationPopup = () => {
+    const confirmationEls = document.getElementsByClassName("c-thermometer-popup");
+    if (confirmationEls.length > 0) {
+        confirmationEls[0].classList.replace("hide", "show");
+    }
+};
+
+/** Hide the feedback modal */
+const hideThermometerModal = () => {
+    const modalEls = document.getElementsByClassName("c-thermometer-modal__container");
+    if (modalEls.length > 0) {
+        modalEls[0].classList.replace("show", "hide");
+    }
+};
+
+/** Show the feedback modal */
+const showThermometerModal = () => {
+    const modalEls = document.getElementsByClassName("c-thermometer-modal__container");
+    if (modalEls.length > 0) {
+        modalEls[0].classList.replace("hide", "show");
+    }
+};
+
+/** Reset the feedback textarea and the character count */
+const resetThermometerTextarea = () => {
+    const textareaEl = document.getElementById("c-thermometer-modal__textarea");
+    if (textareaEl) {
+        textareaEl.value = "";
+    }
+    const counterSpanEl = document.getElementById("c-thermometer-modal__counter-span");
+    if (counterSpanEl) {
+        counterSpanEl.innerText = "0";
+    }
+};
+
+/** Cancel button from feedback modal; hide the modal and reset the emoji states. */
+const cancelThermometerModal = (event) => {
+    hideThermometerModal();
+    resetEmojiStates();
+};
+
+/** Submit button from feedback modal; submit the feedback, reset the feedback fields, and show a confirmation */
+const submitThermometerModal = (event) => {
+    let currentString = "";
+    const textareaEl = document.getElementById("c-thermometer-modal__textarea");
+    if (textareaEl) {
+        currentString = textareaEl.value;
+    }
+    // console.log(`submitThermometerModal(): currentString=${currentString}, eventValue=${eventValue}`);
+    if (currentString !== "" && eventValue > 0) {
+        // Submit DataLayer Event
+        const dataLayer = window.dataLayer || [];
+        dataLayer.push({
+            event: 'rateThisPage',
+            eventLabel: rating,
+            eventValue,
+            eventFeedback: currentString
+        });
+        // Submit Rudderstack event if possible
+        if (typeof (rudderanalytics) !== 'undefined') {
+            rudderanalytics.track("feedback_submitted", {
+                label: rating,
+                rating: eventValue,
+                feedback: currentString
+            });
+        }
+        hideThermometerModal();
+        resetEmojiStates();
+        resetThermometerTextarea();
+        // reset the eventValue
+        eventValue = 0;
+        showConfirmationPopup();
+        setTimeout(() => {
+            hideConfirmationPopup();
+        }, 3000);
+    }
+};
+
+/** Add click handlers to the buttons in the feedback modal */
+const addModalButtons = () => {
+    const cancelButtonEl = document.getElementById("c-thermometer-modal__footer-cancel");
+    if (cancelButtonEl) {
+        cancelButtonEl.addEventListener("click", cancelThermometerModal);
+    }
+    const submitButtonEl = document.getElementById("c-thermometer-modal__footer-submit");
+    if (submitButtonEl) {
+        submitButtonEl.addEventListener("click", submitThermometerModal);
+    }
+};
 
 /**
  * Update the textarea length display with the current length of the textarea.
@@ -58,52 +151,36 @@ const cThermometerUpdateTextLength = (event) => {
     }
 };
 
-$('body').on('click', function () {
-    $('.c-thermometer__popup').hide();
-});
+/** Add a document click handler to hide any visible modals or confirmation popups */
+const addDocumentClick = () => {
+    document.addEventListener('click', (e) => {
+        hideConfirmationPopup();
+        hideThermometerModal();
+        resetEmojiStates();
+    });
+};
 
-$('body').on('click', '.c-thermometer-popup__close', function () {
-    $(this).parent().fadeOut('fast');
-});
-
-$('body').on('click', '.c-thermometer__popup', function (e) {
-    e.stopImmediatePropagation();
-});
-
-$('body').on('click', '.c-thermometer__trigger', function (e) {
-    e.stopImmediatePropagation();
-});
-
-// Click Event for Ratings
-$('body').on('click', '.c-thermometer__emojis a.rate-this-page-action', function () {
-    const click_elem = $(this);
-    click_elem.addClass('selected');
-
-    // UX Update
-    $('.c-thermometer-modal__container').fadeIn();
-
-    // Prepare DataLayer Vars
-    rating = click_elem.attr('data-rating');
-
-    switch (rating) {
-        case 'Yes':
-            eventValue = 3;
-            break;
-        case 'Somewhat':
-            eventValue = 2;
-            break;
-        case 'No':
-            eventValue = 1;
-            break;
-    }
-});
-
-// install textarea text length handler
 document.addEventListener("DOMContentLoaded", (event) => {
+    // Add a handler to the textarea which updates the character count display
     const textareaEl = document.getElementById("c-thermometer-modal__textarea");
     if (textareaEl) {
         textareaEl.addEventListener("input", cThermometerUpdateTextLength);
-    } else {
-        console.error("Feedback widget: could not attach event handler to textarea; feedback text counter will not function.");
     }
+    // Stop the propagation of click events for particular divs. This allows the modal to remain visible
+    // when clicking inside it.
+    const modalContentEls = document.getElementsByClassName("c-thermometer-modal__content");
+    if (modalContentEls.length > 0) {
+        modalContentEls[0].addEventListener('click', e => e.stopImmediatePropagation());
+    }
+    const modalTriggerEls = document.getElementsByClassName("c-thermometer__trigger");
+    if (modalTriggerEls.length > 0) {
+        modalTriggerEls[0].addEventListener('click', e => e.stopImmediatePropagation());
+    }
+    const popupEls = document.getElementsByClassName("c-thermometer__popup");
+    if (popupEls.length > 0) {
+        popupEls[0].addEventListener('click', e => e.stopImmediatePropagation());
+    }
+    addEmojiClick();
+    addDocumentClick();
+    addModalButtons();
 });
